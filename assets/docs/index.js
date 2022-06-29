624ec2eeb9a02796740004c790c22b3bc4b7fb26
Paul M. Summitt <psummitt@gmail.com>
2022-06-29 13:55:06 -0500

    new approach

--- assets/docs/index.js
+++ assets/docs/index.js
@@ -1,42 +1,42 @@
-let tasks = new TaskListManager;
+let tasks = new TaskManager();
 
 tasks.load();
 tasks.render();
 
-const newTaskNameInput = document.getElementById("taskName");
-const newTaskDescriptionInput = document.getElementById("taskDescription");
-const newAssignedInput = document.getElementById("assignedTo");
-const newDueDateInput = document.getElementById("dueDate");
+const newTaskNameInput = document.querySelector("#TaskName");
+const newTaskDescriptionInput = document.querySelector("#newTaskDescriptionInput");
+const newAssignedInput = document.querySelector("#Assigned");
+const newDueDateInput = document.querySelector("#newDueDateInput");
 
 const validFormFieldInput = (e) => {
 	e.preventDefault();
-	const taskName = newTaskNameInput.value;
-	const taskDescription = newTaskDescriptionInput.value;
-	const assignedTo = newAssignedInput.value;
+	const name = newTaskNameInput.value;
+	const description = newTaskDescriptionInput.value;
+	const assigned = newAssignedInput.value;
 	const dueDate = newDueDateInput.value;
 
-	tasks.addTask(taskName, taskDescription, assignedTo, dueDate);
+	tasks.addTask(name, description, assigned, dueDate);
 	tasks.render();
 	tasks.save();
 
 	document.getElementById("myForm").reset();
 };
 
-document.getElementById("btnAddTask").addEventListener("click", function() {
-	const taskName = newTaskNameInput.value;
-	const taskDescription = newTaskDescriptionInput.value;
-	const assignedTo = newAssignedInput.value;
+document.getElementById("btn").addEventListener("click", function() {
+	const name = newTaskNameInput.value;
+	const description = newTaskDescriptionInput.value;
+	const assigned = newAssignedInput.value;
 	const dueDate = newDueDateInput.value;
 
-	if (taskName === "") {
+	if (name === "") {
 		document.getElementById("error").innerHTML = `<div id="error" class="alert alert-primary" role="alert">
 	Please input name field!
   </div>`;
-	} else if (taskDescription === "") {
+	} else if (description === "") {
 		document.getElementById("error").innerHTML = `<div id="error" class="alert alert-primary" role="alert">
 	Please input description field!
   </div>`;
-	} else if (assignedTo === "") {
+	} else if (assigned === "") {
 		document.getElementById("error").innerHTML = `<div id="error" class="alert alert-primary" role="alert">
 	Please input assigned field!
   </div>`;
@@ -49,12 +49,12 @@ Please input the Due Date field!
 	}
 });
 
-// Event listener for mark as done button.
+// Event listerner for mark as done button.
 
-const taskList = document.qetElementById("displayCard2");
+const taskList = document.querySelector("#taskList");
 
 taskList.addEventListener("click", (event) => {
-	if (event.target.classList.contains("btnMarkAsDone")) {
+	if (event.target.classList.contains("done-button")) {
 		let parentTask = event.target.parentNode.parentNode.parentNode;
         let taskId = parseInt(parentTask.getAttribute("data-task-id"));
         let task = tasks.getTaskById(taskId);
@@ -69,7 +69,7 @@ taskList.addEventListener("click", (event) => {
         tasks.save();
 	}
 
-	if (event.target.classList.contains("btnDelete")) {
+	if (event.target.classList.contains("delete-button")) {
 		let parentTask = event.target.parentNode.parentNode.parentNode.parentNode;
 		let taskId = parseInt(parentTask.getAttribute("data-task-id"));
 		document.getElementById("taskList").innerHTML = "";
